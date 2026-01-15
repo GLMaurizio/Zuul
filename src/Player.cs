630939b1f5883f11643bd0474e8d2f68b502e083
Maurizio <Licherigiovannimaurizio@gmail.com>
2026-01-15 13:14:56 +0100

    Fixed player

---
+++ src/Player.cs
@@ -0,0 +1,51 @@
+class Player
+{ 
+    // Fields
+    private int health;
+    
+    // auto property
+    public Room CurrentRoom { get; set; }
+
+    // constructor
+    public Player()
+    {
+        CurrentRoom = null;
+
+        health = 100;
+    }
+
+    // methods
+    public void Damage(int amount){
+        health--;
+        health--;
+        health--;
+        health--;
+        health--;
+
+        return;
+    }
+
+    // Health
+    public void Heal(int amount){
+        health++;
+        health++;
+        health++;
+        health++;
+        health++;
+
+        return;
+    }
+
+    public bool IsAlive()
+    {
+
+    if (health != 0) {
+        Console.WriteLine("The player is alive with " + health + " health");
+        return true;}
+    
+    else {
+        Console.WriteLine("The player is dead and has no remaining health points left.");
+        return false;}
+    }
+
+}
